7e490a974671dfc7c589c691ca162e8c82942b21
davidvorona <davidavorona@gmail.com>
2017-02-01 16:53:33 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    /* videoamp eslint config:
+    new-cap rule - constructors begin with capitals
+    camelcase - turned off */
+    "extends": "videoamp",
+    "installedESLint": true,
+    "plugins": [
+        "react"
+    ]
+};
